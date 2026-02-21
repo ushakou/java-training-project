22e6cca47fcfb80a9f42171ccad3f8a35107b470
admin <111@111>
2026-02-21 13:16:07 +0300

    lesson-2 add Task3

---
+++ src/main/java/lesson2/Task3.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+/*
+Задача 3:
+Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно.
+ */
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        for (int i = 10; i <= 20; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
